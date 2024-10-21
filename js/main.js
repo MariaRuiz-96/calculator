1fea82d74e9c23a991fb2041ee0fd3fa1e8f2200
MariaRuiz-96 <marijo.ruch1996@gmail.com>
2024-10-21 11:07:18 -0600

    Creacion de la primera y segunda variable
    Se creo la primera y segunda variable que nos servira para poder tomar en numero introducido para la matematica

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 //* Cambia el numero dependiendo del boton seleccionado
 
-let display = document.querySelector("#displayNumber");
+let firstNumber = ""
+let secondNumber = ""
+let isOperatorSelect = false //- nos ayuda para saber si ya fue seleccionado o no el operador
 
+let display = document.querySelector("#displayNumber");
 let numbers = document.querySelectorAll(".numbers");
 
 let diplayNumber = numbers.forEach((number) => {
     number.addEventListener("click", ()=>{
         let value = number.value
 
-        display.textContent = value
+        if(!isOperatorSelect){
+            firstNumber += value;
+            display.textContent = firstNumber;
+        }else{
+            secondNumber += value
+            display.textContent = firstNumber;
+        }
     })
 });
-
 
